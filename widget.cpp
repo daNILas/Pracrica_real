9e50770266ba469b7a104fdbccef23eaf63237c0
daNILas <di--na@yandex.ru>
2015-07-02 15:21:38 -1200

    design

--- widget.cpp
+++ widget.cpp
@@ -1,11 +1,26 @@
 #include "widget.h"
 
-Widget::Widget(QWidget *parent)
-    : QWidget(parent)
+MyWindow::MyWindow(QWidget *parent) : QDialog(parent)
 {
-}
+    lbl=new QLabel("Enter");
+    line=new QLineEdit;
+    cb1=new QCheckBox("Новое окно");
+    ok = new QPushButton("Показать");
+    close = new QPushButton("Выход");
+    QHBoxLayout *layout = new QHBoxLayout;
+    layout->addWidget(lbl);
+    layout->addWidget(line);
+    QVBoxLayout *left = new QVBoxLayout;
+    left->addLayout(layout);
+    left->addWidget(cb1);
+    QVBoxLayout *right = new QVBoxLayout;
+    right->addWidget(ok);
+    right->addWidget(close);
+    QHBoxLayout *mains = new QHBoxLayout;
+    mains->addLayout(left);
+    mains->addLayout(right);
 
-Widget::~Widget()
-{
+
+    setLayout(mains);
 
 }
--- widget.h
+++ widget.h
@@ -1,15 +1,34 @@
 #ifndef WIDGET_H
 #define WIDGET_H
 
+
+#include <QDialog>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QCheckBox>
 #include <QWidget>
 
-class Widget : public QWidget
+
+class MyWindow : public QDialog
 {
     Q_OBJECT
 
 public:
-    Widget(QWidget *parent = 0);
-    ~Widget();
+    MyWindow (QWidget *parent=0);
+
+private:
+    QLabel *lbl;
+    QLineEdit *line;
+    QPushButton *ok;
+    QPushButton *close;
+    QCheckBox *cb1;
+
+
+
+
 };
 
 #endif // WIDGET_H
